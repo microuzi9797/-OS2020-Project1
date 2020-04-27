c52f65332f23c68e3ee01293e410ce9d9e5d0acc
microuzi9797 <ken97971@yahoo.com.tw>
2020-04-27 18:00:40 +0800

    kernel_files

--- kernel_files/get_time.c
+++ kernel_files/get_time.c
@@ -7,7 +7,6 @@ asmlinkage int sys_get_time(unsigned long *sec, unsigned long *nsec)
 {
     struct timespec t;
     getnstimeofday(&t);
-
     *sec = t.tv_sec;
     *nsec = t.tv_nsec;
     return 0;
